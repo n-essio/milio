f8db001f7fa11d463413684c4e9193a31466891f
fiorenzo pizza <fiorenzino@gmail.com>
2021-01-06 23:13:51 +0100

    first commit

--- src/main/java/io/milio/service/rs/Milio.java
+++ src/main/java/io/milio/service/rs/Milio.java
@@ -8,17 +8,21 @@ import io.vertx.core.http.HttpMethod;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ApplicationScoped
 public class Milio {
 
     Logger logger = Logger.getLogger(getClass());
 
+    AtomicInteger atomicInteger = new AtomicInteger(0);
+
     @Route(path = "/:id", produces = "application/json", methods = HttpMethod.POST)
     Result hello(@Body Location location, @Param String id, @Header("authorization") String authorization) {
         logger.info("id:" + id);
         logger.info("location:" + location);
         logger.info("authorization:" + authorization);
+        logger.info("count:" + atomicInteger.addAndGet(1));
         logger.info("-------------------");
         return new Result("ok");
     }
